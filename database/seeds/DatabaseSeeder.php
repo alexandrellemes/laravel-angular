bba995f7f76880e0e41d306a5173815c3c4ce8ac
Alexandre LLemes <alexandre-pl@segplan.go.gov.br>
2018-02-16 09:33:53 -0200

    Create factory of the 15 users.

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,7 +11,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(UsersTableSeeder::class);
+//        $this->call(UsersTableSeeder::class);
+
+        factory(\App\User::class, 15)->create();
 
         factory(\App\Contato::class, 10)->create();
     }
